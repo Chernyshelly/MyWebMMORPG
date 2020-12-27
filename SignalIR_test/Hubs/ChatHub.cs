0921a204675cb50d6999360a3c66ac5105ffcd16
Chernyshelly <andreycherni@gmail.com>
2020-12-28 00:46:14 +0300

    Добавьте файлы проекта.

---
+++ SignalIR_test/Hubs/ChatHub.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using WebMMO.Models;
+
+namespace WebMMO.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Register(string user, string message)
+        {
+            bool canRegister = true;
+            string reply;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    if (u.Name == user)
+                    {
+                        canRegister = false;
+                    }
+                }
+                if (canRegister)
+                {
+                    User newUser = new User { Name = user, Age = 33 };
+                    db.Users.Add(newUser);
+                    db.SaveChanges();
+                    reply = "successReg";
+                }
+                else
+                {
+                    reply = "failReg";
+                }
+            }
+            await Clients.Caller.SendAsync("ReceiveMessage", user, reply);
+        }
+        public async Task Login(string user, string message)
+        {
+            string reply = $"{user} is trying to login with password: {message}";
+            await Clients.Caller.SendAsync("ReceiveMessage", user, reply);
+        }
+    }
+}
---
+++ SignalIR_test/Models/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebMMO.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-6SFGD2C;Database=TestDB;Trusted_Connection=True;");
+        }
+    }
+}
