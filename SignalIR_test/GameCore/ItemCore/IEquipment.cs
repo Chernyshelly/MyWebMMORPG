1cce2403b0183fd380cf1d87c54c7542112885cc
Chernyshelly <andreycherni@gmail.com>
2021-01-02 19:45:25 +0300

    Created interfaces for Equipment, Weapons and Effects

---
+++ SignalIR_test/GameCore/ItemCore/IEquipment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMMO.GameCore.EffectCore;
+
+namespace WebMMO.GameCore.ItemCore
+{
+    public interface IEquipment : IItem
+    {
+        public int Level { get; set; }
+        public int StrengthBonus { get; set; }
+        public int AgilityBonus { get; set; }
+        public int IntelligenceBonus { get; set; }
+        public List<IEffect> Effects { get; set; }
+    }
+}
