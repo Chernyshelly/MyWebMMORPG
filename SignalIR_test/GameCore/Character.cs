beb04cc70fda7494895cce5b1ed50a7a839cd31c
Chernyshelly <andreycherni@gmail.com>
2021-02-25 21:27:55 +0300

    freed user from Characters parts (now there must be db for characters and user now gains possibility to have 2+ characters)

--- SignalIR_test/GameCore/Character.cs
+++ SignalIR_test/GameCore/Character.cs
@@ -6,9 +6,21 @@ using WebMMO.Models;
 
 namespace WebMMO.GameCore
 {
-    public class Character : User
+    public enum CharacterClass
+    {
+        Bogatyr,
+        Rogue,
+        Priest
+    }
+    public class Character
     {
         public int MaxHealth { get; set; }
         public int MaxMana { get; set; }
+        public int Level { get; set; }
+        public int Experience { get; set; }
+        public CharacterClass Class { get; set; }
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Intelligence { get; set; }
     }
 }
